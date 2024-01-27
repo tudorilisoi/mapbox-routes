8335367b21f287e9fa0609d8ac5fa0eae64a64b4
Tudor Ilisoi <tudorilisoi@yahoo.com>
2024-01-27 16:06:42 +0200

    add departure layer
    Signed-off-by: Tudor Ilisoi <tudorilisoi@yahoo.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './App.css'
-import Map from 'react-map-gl'
-
+import Map, { Source, Layer } from 'react-map-gl'
+import type { CircleLayer } from 'react-map-gl'
+import * as turf from '@turf/turf'
 
 // appx centero of Dublin
 const initialCoords = [-6.26031, 53.349805]
+const departureCoords = [-6.26031, 53.349805]
+
+const depStyle: CircleLayer = {
+  id: 'departure',
+  type: 'circle',
+  paint: {
+    'circle-radius': 15,
+    'circle-color': 'lightblue',
+    'circle-stroke-color': '#3887be',
+    'circle-stroke-width': 3,
+  },
+}
+
+const warehouse = turf.featureCollection([turf.point(departureCoords)])
 
 function App() {
   return (
@@ -19,7 +34,11 @@ function App() {
         }}
         style={{ width: 600, height: 400 }}
         mapStyle="mapbox://styles/mapbox/light-v11"
-      />
+      >
+        <Source id="my-data" type="geojson" data={warehouse}>
+          <Layer {...depStyle} />
+        </Source>
+      </Map>
     </div>
   )
 }
